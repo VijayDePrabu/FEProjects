7b423c13dde3049b533d2f4e4403dca866c0efc9
Vijay De Prabu <vijayde3@gmail.com>
2021-02-13 21:44:43 +0530

    Update constants.js

--- base/constants.js
+++ base/constants.js
@@ -1,8 +1,13 @@
+const GOOGLE_API_KEY = "AIzaSyDExmNY40i0jMDsUfDIgcksjYZmr4L0Ems";
+
+const proxy_url = 'https://cors-anywhere.herokuapp.com/';
+
 const CITY_PLACE_ID_BASE_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyDExmNY40i0jMDsUfDIgcksjYZmr4L0Ems&input={city_name}&inputtype=textquery";
 
 const CITY_DETAILS_BASE_URL = "https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyDExmNY40i0jMDsUfDIgcksjYZmr4L0Ems&place_id={place_id}";
 
-const CITY_PHOTOS_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=640&photoreference=ATtYBwLfz0osQTQX2yzeJd0zVLhXGANyQg_MTT3gBRBKgbxQ6O67DvsNqkHc_t7aXazDSGjLoxF_dBtsY0c6FIVY1lLpFevMJ9HgHT82y7_3g1YTsboOCud-z0kLPM3AdOdWSi76LPYCJe-_lFqZJRlhJ8vHHA81MJ-lZSQhjA-9TrJFfG6U&key=AIzaSyDExmNY40i0jMDsUfDIgcksjYZmr4L0Ems"
+const CITY_PHOTOS_URL = "https://maps.googleapis.com/maps/api/place/photo?photoreference={photoreference}&sensor=false&maxheight=500&maxwidth=500&key=AIzaSyDExmNY40i0jMDsUfDIgcksjYZmr4L0Ems"
+ //"https://maps.googleapis.com/maps/api/place/photo?maxwidth=640&photoreference={photoreference}"
 
 const RAZORPAY_IFSC_BASE_URL = "https://ifsc.razorpay.com/";
 // stock search
@@ -22,6 +27,10 @@ const STOCK_CHART_DATA_BASE_URL =  "https://sandbox.iexapis.com/stable/stock/twt
 const WEATHER_API_KEY = "eee5d0619d3cb02ed044cbe2e0d78ef3";
 
 // weather for geo location
-const WEATHER_GEOLOCATION_API = " http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid="+WEATHER_API_KEY;
+const WEATHER_GEOLOCATION_API = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid="+WEATHER_API_KEY;
+
+const WEATHER_ICON_BASE_URL = "https://openweathermap.org/img/w/{weather-icon}.png";
 
 let methodVal = "GET";
+
+let useProxy = false;
